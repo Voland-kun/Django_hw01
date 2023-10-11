12215682d4b67f7f0f189b54def6567dc4e0ab32
Voland <voland.artist@gmail.com>
2023-10-11 05:26:16 +0400

    hw04

--- hw02/hw02app/models.py
+++ hw02/hw02app/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.IntegerField()
     date = models.DateTimeField(auto_now=True)
+    photo = models.ImageField(upload_to='photo/', blank=True, null=True)
 
     def __str__(self):
         return f'Name: {self.name}, description: {self.description}, price: {self.price}, quantity: {self.quantity},' \
--- hw02/hw02app/views.py
+++ hw02/hw02app/views.py
@@ -1,26 +1,15 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from datetime import timedelta
 
 from .models import Customer, Order, Product
+from .forms import ProductForm
 
 
 def index(request):
-    html = """
-        <html>
-        <head>
-            <title>Главная страница</title>
-        </head>
-        <body>
-            <h1>Главная страница</h1>
-            <p>Главная страница</p>
-        </body>
-        </html>
-    """
-    return HttpResponse(html)
+    return render(request, 'hw02app/index.html')
 
 
 def ordered_products(request, customer_id, days):
@@ -37,3 +26,36 @@ def ordered_products(request, customer_id, days):
 def customer_name(request, customer_id):
     customer = get_object_or_404(Customer, pk=customer_id)
     return render(request, "hw02app/customer.html", {"customer": customer})
+
+
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('products')
+    else:
+        form = ProductForm()
+    return render(request, 'hw02app/product_create.html', {'form': form})
+
+
+def edit_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_page', product_id=product_id)
+    else:
+        form = ProductForm(instance=product)
+    return render(request, 'hw02app/product_edit.html', {'form': form})
+
+
+def products_list(request):
+    products = Product.objects.all()
+    return render(request, "hw02app/products.html", {"products": products})
+
+
+def product_page(request, product_id):
+    product_ = get_object_or_404(Product, pk=product_id)
+    return render(request, "hw02app/product.html", {"product": product_})
