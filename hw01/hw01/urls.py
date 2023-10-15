5d677f1d8fa6ddc6d8fdcfaa3697516abd50afc3
Voland <voland.artist@gmail.com>
2023-10-15 18:41:37 +0400

    hw06

--- hw01/hw01/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('amin/', admin.site.urls),
-    path('', include('hw01app.urls')),
-]
--- hw02/hw02/settings.py
+++ hw02/hw02/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p%i0_+(828r5#g+e&j$!+!n5n*8#%-)$lm$b!+_m%%t_kt^igy'
+# SECRET_KEY = 'django-insecure-p%i0_+(828r5#g+e&j$!+!n5n*8#%-)$lm$b!+_m%%t_kt^igy'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
 
-ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = [
+    'VolandKun.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -78,8 +85,15 @@ WSGI_APPLICATION = 'hw02.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'VolandKun$default',
+        'USER': 'VolandKun',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'VolandKun.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4' ;SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -119,6 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
